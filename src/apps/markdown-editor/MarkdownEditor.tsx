fc54f34c04fcaea16f2442e9c05cdff66a7d2726
Gurleen Sethi <sarusethi@rocketmail.com>
2020-09-03 18:56:07 -0400

    Sync editor and preview scroll.

--- src/apps/markdown-editor/MarkdownEditor.tsx
+++ src/apps/markdown-editor/MarkdownEditor.tsx
@@ -4,6 +4,8 @@ import marked from "marked";
 
 const MarkdownEditor: FunctionComponent = () => {
   const [markdown, setMarkdown] = useState<string>("# sup");
+  const previewRef = useRef<HTMLDivElement | null>(null);
+  const markdownRef = useRef<HTMLTextAreaElement | null>(null);
 
   const handleTextChange = useCallback(
     (e: React.ChangeEvent<HTMLTextAreaElement>) => {
@@ -13,14 +15,37 @@ const MarkdownEditor: FunctionComponent = () => {
     [setMarkdown]
   );
 
-  console.log(marked(markdown));
+  const handlePreviewScrollPosition = useCallback(
+    (e: React.UIEvent<HTMLDivElement, UIEvent>) => {
+      if (previewRef.current && markdownRef.current) {
+        markdownRef.current.scrollTop = previewRef.current.scrollTop;
+      }
+    },
+    []
+  );
+
+  const handleMarkdownScrollPosition = useCallback(
+    (e: React.UIEvent<HTMLTextAreaElement, UIEvent>) => {
+      if (previewRef.current && markdownRef.current) {
+        previewRef.current.scrollTop = markdownRef.current.scrollTop;
+      }
+    },
+    []
+  );
 
   return (
     <div className={styles.app}>
-      <textarea onChange={handleTextChange} value={markdown} />
+      <textarea
+        ref={markdownRef}
+        onChange={handleTextChange}
+        value={markdown}
+        onScroll={handleMarkdownScrollPosition}
+      />
       <div
         className={styles.preview}
+        ref={previewRef}
         dangerouslySetInnerHTML={{ __html: marked(markdown) }}
+        onScroll={handlePreviewScrollPosition}
       />
     </div>
   );
